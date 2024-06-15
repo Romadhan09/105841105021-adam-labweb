b5d2a23f210401ea18b27600c435a5a04c5f21db
Romadhan09 <105841105021@student.unismuh.ac.id>
2024-06-15 10:01:04 +0800

    Button

--- App.js
+++ App.js
@@ -4,33 +4,51 @@ import React from "react";
 const App = () => {
   return(
     <View style={{
-      flex:1,
-      marginTop:300
+      marginTop:400
     }}>
 
-    
     <View style={{
-      //flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+      flex: 1,
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+      marginLeft:70,
     }}>
       <View style={{
         width:100,
-        height:100,
+        height:50,
         backgroundColor:'red',
+        borderRadius: 20,
+        justifyContent: 'center'
       }}>
+        <Text style={{
+          fontSize: 20,
+          color: 'grey',
+          textAlign:'center',
+          fontWeight: 'bold',
+        }}>Login
+        </Text>
+
       </View>
     </View>
     <View style={{
-      //flex:1,
-      justifyContent:'center',
-      alignItems: 'center',
+      flex:1,
+      justifyContent:'flex-start',
+      alignItems: 'flex-end',
+      marginRight: 70
     }}>
       <View style={{
         width:100,
-        height:100,
+        height:50,
         backgroundColor: 'blue',
+        borderRadius:20,
+        justifyContent:'center',
       }}>
+        <Text style={{
+          fontSize:20,
+          color:'grey',
+          textAlign:'center',
+          fontWeight:'bold',
+        }}>Sing Up</Text>
 
       </View>
     </View>
