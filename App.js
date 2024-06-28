8d638c9513702cda47ace981794b0b23b8312723
Romadhan09 <105841105021@student.unismuh.ac.id>
2024-06-28 11:36:54 +0800

    useFont

--- App.js
+++ App.js
@@ -1,61 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Text, View } from 'react-native';
+import { useFonts} from 'expo-font';
 
-const App = () => {
-  return(
+export default function App() {
+  const [fontLoaded] = useFonts({
+    'MetroBlack' : require('./assets/Fonts/Metropolis-Black.otf'),
+    'MetroBold' : require('./assets/Fonts/Metropolis-Bold.otf'),
+    'MetroLight' : require('./assets/Fonts/Metropolis-Light.otf'),
+    'MetroSemiBold' : require('./assets/Fonts/Metropolis-SemiBold.otf'),
+  });
+  if(!fontLoaded) {
+    return <Text>Font tidak ditemukan  ....</Text>
+  }
+  return (
     <View style={{
-      marginTop:400
+      flex: 1, 
+      justifyContent : 'center',
+      alignItems : 'center',
     }}>
-
-    <View style={{
-      flex: 1,
-      justifyContent: 'flex-start',
-      alignItems: 'flex-start',
-      marginLeft:70,
-    }}>
-      <View style={{
-        width:100,
-        height:50,
-        backgroundColor:'red',
-        borderRadius: 20,
-        justifyContent: 'center'
-      }}>
-        <Text style={{
-          fontSize: 20,
-          color: 'grey',
-          textAlign:'center',
-          fontWeight: 'bold',
-        }}>Login
-        </Text>
-
-      </View>
+      <Text style={{ 
+        fontFamily: 'MetroBlack'
+      }}>Font Lebar dan Tebal</Text>
+       <Text style={{ 
+        fontFamily: 'MetroBold'
+      }}>Font Tebal</Text>
+       <Text style={{ 
+        fontFamily: 'MetroLight'
+      }}>Font Kecil</Text>
+       <Text style={{ 
+        fontFamily: 'MetroSemiBold'
+      }}>Font Kecil dan Tebal</Text>
     </View>
-    <View style={{
-      flex:1,
-      justifyContent:'flex-start',
-      alignItems: 'flex-end',
-      marginRight: 70
-    }}>
-      <View style={{
-        width:100,
-        height:50,
-        backgroundColor: 'blue',
-        borderRadius:20,
-        justifyContent:'center',
-      }}>
-        <Text style={{
-          fontSize:20,
-          color:'grey',
-          textAlign:'center',
-          fontWeight:'bold',
-        }}>Sing Up</Text>
-
-      </View>
-    </View>
-    </View>
- 
-  )
+  );
 }
-
-
-export default App
